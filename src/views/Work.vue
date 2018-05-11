1a9bfb61e85ffd060cbf0cc583b3ef82928e6957
Pence <matt@ohiosveryown.co>
2018-05-11 10:55:14 -0400

    Work Reset Baseline

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -5,8 +5,8 @@
     <!-- ✋🏼 -->
     <section class="left">
       <header>
-        <h1 class="mb-2 uc f-xl f-mabry">Hi, I’m Matt—a product designer living in Atlanta, Ga.</h1>
-        <h2 class="meas-8 uc f-lg f-mabry">I’m currently researching design systems &amp; machine learning.</h2>
+        <h1 class="mb-2 uc f-xl f-mabry-med">Hi, I’m Matt—a product designer living in Atlanta, Ga.</h1>
+        <h2 class="meas-7 f-lg f-travels-med">I’m currently researching design systems &amp; machine learning.</h2>
       </header>
 
       <h6 class="mb-6 uc f-sm f-basismono">
@@ -18,40 +18,40 @@
         <li class="sp__galleries">
           <router-link to="/supersymmetry">
             <h6 class="mb-1 op-5 uc f-sm f-basismono">ux/ui</h6>
-            <h2 class="mb-1 uc f-lg f-mabry">ShootProof Galleries</h2>
-            <h3 class="f-md f-mabry">The biggest feature from my 9-5, used by a bunch of folks; Got to pay them billz.</h3>
+            <h2 class="mb-1 uc f-lg f-mabry-med">ShootProof Galleries</h2>
+            <h3 class="f-md f-system">The biggest feature from my 9-5, used by a bunch of folks; Got to pay them billz.</h3>
           </router-link>
         </li>
 
         <li>
           <router-link to="/supersymmetry">
             <h6 class="mb-1 op-5 uc f-sm f-basismono">ux/ui/dev</h6>
-            <h2 class="mb-1 uc f-lg f-mabry">Supersymmetry</h2>
-            <h3 class="f-md f-mabry">A self-initiated project focusing on engineering, buildings, interior desing—basically spaces.</h3>
+            <h2 class="mb-1 uc f-lg f-mabry-med">Supersymmetry</h2>
+            <h3 class="f-md f-system">A self-initiated project focusing on engineering, buildings, interior desing—basically spaces.</h3>
           </router-link>
         </li>
 
         <li>
           <router-link to="/supersymmetry">
             <h6 class="mb-1 op-5 uc f-sm f-basismono">ux/ui/dev</h6>
-            <h2 class="mb-1 uc f-lg f-mabry">Questions About Space</h2>
-            <h3 class="f-md f-mabry">A side project about space. Space is cool. And Vast. But mostly cold.</h3>
+            <h2 class="mb-1 uc f-lg f-mabry-med">Questions About Space</h2>
+            <h3 class="f-md f-system">A side project about space. Space is cool. And Vast. But mostly cold.</h3>
           </router-link>
         </li>
 
         <li>
           <router-link to="/supersymmetry">
             <h6 class="mb-1 op-5 uc f-sm f-basismono">ux/ui/dev</h6>
-            <h2 class="mb-1 uc f-lg f-mabry">Vibes</h2>
-            <h3 class="f-md f-mabry">I know what you’re thinking, “What about some mobile stuff?” I got you.</h3>
+            <h2 class="mb-1 uc f-lg f-mabry-med">Vibes</h2>
+            <h3 class="f-md f-system">I know what you’re thinking, “What about some mobile stuff?” I got you.</h3>
           </router-link>
         </li>
 
         <li>
           <router-link to="/supersymmetry">
             <h6 class="mb-1 op-5 uc f-sm f-basismono">ux/ui</h6>
-            <h2 class="mb-1 uc f-lg f-mabry">ShootProof Uploader</h2>
-            <h3 class="f-md f-mabry">Some more product work here. Uploading photos is suprisingly tough.</h3>
+            <h2 class="mb-1 uc f-lg f-mabry-med">ShootProof Uploader</h2>
+            <h3 class="f-md f-system">Some more product work here. Uploading photos is suprisingly tough.</h3>
           </router-link>
         </li>
       </ul>
@@ -72,7 +72,7 @@
     font-size: 1.8rem;
     line-height: 1.1;
     @include breakpoint(sm) { font-size: 2rem; }
-    @include breakpoint(md) { font-size: 3rem; }
+    @include breakpoint(md) { font-size: 2.7rem; }
   }
 
   .f-lg {
@@ -84,6 +84,7 @@
   .f-md {
     font-size: 1.7rem;
     line-height: 1.3;
+    letter-spacing: -.5px;
   }
 
   .f-sm {
@@ -115,7 +116,7 @@
   }
 
   ul > li {
-    @include breakpoint(mdl) { margin-bottom: 4.8rem; width: grid-width(8.5); }
+    @include breakpoint(mdl) { margin-bottom: 5.2rem; width: grid-width(8.5); }
   }
 
   ul > li:last-child { margin-bottom: 9.6rem; }
