335810caca79ea3299c083cd0a7b61da3aa4829d
ohiosveryown <matt@ohiosveryown.co>
2018-05-14 16:17:07 -0400

    Updating Spacing for Work Detail

--- src/views/Supersymmetry.vue
+++ src/views/Supersymmetry.vue
@@ -2,155 +2,43 @@
 <template>
   <main class="mw__lg">
 
+    <!-- hero 👰🏻 -->
     <section class="hero">
       <!-- header 💆 -->
-      <header class="uc">
+      <header class="hero__header uc">
         <h4 class="mb-0 f-sm f-basismono">ux/dev</h4>
-        <h1 class="mb-1 f-xl f-mabry-med">Supersymmetry</h1>
-        <h2 class="f-lg f-travels-med">In the fall of 2016 I began a self-initiated project to design &amp; develop a photo-blog in Vue JS focusing on architecture, interior design &amp; structural engineering.</h2>
+        <h1 class="mb-2 f-xl f-mabry-med">Supersymmetry</h1>
+         <h2 class="f-lg f-travels-med">In the fall of 2016 I began a self-initiated project to design &amp; develop a photo-blog in Vue JS focusing on architecture, interior design &amp; structural engineering.</h2>
       </header>
-      <!-- scroll directions ⬇️ -->
-      <div class="hero__scroll hide-sm">
+      <!-- instructions 💬 -->
+      <nav class="hero__scroll">
         <h2 class="f-md tar uc f-travels-med">Scroll Down</h2>
-      </div>
-      <!-- meta info -->
-      <div class="hero__meta">
+      </nav>
+      <!-- date & stuff 📆 -->
+      <aside class="hero__meta">
         <h4 class="mb-1 f-sm uc f-basismono">Timeline</h4>
-        <h3 class="mb-3 f-md f-travels-med">Fall 2016 – Spring 2017</h3>
+        <h3 class="mb-4 f-md f-travels-med">Fall 2016 – Spring 2017</h3>
         <h4 class="mb-1 f-sm uc f-basismono">Link</h4>
         <h3 class="f-md f-travels-med">
-          <a class="link btn__tertiary" href="http://supersymmetry.design" target="_blank">http://supersymmetry.design</a>
+          <a class="link btn__tertiary" href="http://supersymmetry.design" target="_blank">supersymmetry.design</a>
         </h3>
-      </div>
-    </section>
-
-    <section class="layout__img">
-      <figure style="background-color: #f3f4fb;">
-        <img src="../assets/__supersymmetry/img/01@2x.png" alt="">
-      </figure>
-      <figcaption class="mt-0 tar">fig. 01</figcaption>
-    </section>
-
-    <section>
-      <article>
-        <h4 class="mb-2 uc f-sm f-basismono">situation</h4>
-        <p>The main challenge was to create a brief repository for information of particular spaces. I wanted Supersymmetry to have brevity but highlight what makes these spaces special &amp; unique.</p>
-        <p>I wanted to structure Supersymmetry this way for the sake of brevity and maintenance, while highlighting what makes these specific spaces special (...that alliteration though 👀👏🏼).</p>
-      </article>
-    </section>
-
-    <section>
-      <ul class="figure__key">
-        <li>
-          <span class="mr-0 f-bold uc">fig. 01:</span>
-          <span>Small List View</span>
-        </li>
-        <li>
-          <span class="mr-0 f-bold uc">fig. 02:</span>
-          <span>Early Design Explorations</span>
-        </li>
-      </ul>
-    </section>
-
-    <section class="layout__img">
-      <figure style="background-color: #f3f3f3;">
-        <img src="../assets/__supersymmetry/img/02@2x.jpg" alt="">
-      </figure>
-      <figcaption class="mt-0 tar">fig. 02</figcaption>
-    </section>
-
-    <section class="layout__copy-img">
-      <aside>
-        <h4 class="mb-2 uc f-sm f-basismono">planning</h4>
-        <p>The majority of the time when I begin a side-project, I start by defining requirements &amp; scope. Here, the first couple of things I outlined were purpose &amp; style.</p>
-        <p>Style outlines creative &amp; developmental scaffolding. The creative principles focus on things like type, color &amp; copywriting style, while development focuses on accessibility, optimization &amp; code writing style.</p>
       </aside>
-      <div class="img__wrapper">
-        <figure><img src="../assets/__supersymmetry/img/03@2x.jpg" alt=""></figure>
-        <figcaption class="mt-0 tar">fig. 03</figcaption>
-      </div>
     </section>
 
-    <section class="layout__img-full">
-      <figure>
-        <img class="img-shadow" src="../assets/__supersymmetry/img/04@2x.jpg" alt="">
+    <!-- full-width image 🖼 -->
+    <section class="img__full">
+      <figure style="background-color: #f3f4fb;">
+        <img src="../assets/__supersymmetry/img/01@2x.png" alt="List View for Small Devices">
       </figure>
-      <figcaption class="mt-0 tar">fig. 04</figcaption>
+      <figcaption class="mt-1 tar">fig. 01</figcaption>
     </section>
 
-    <section>
-      <ul class="figure__key">
-        <li>
-          <span class="mr-0 f-bold uc">fig. 04:</span>
-          <span>Large List View</span>
-        </li>
-        <li>
-          <span class="mr-0 f-bold uc">fig. 05:</span>
-          <span>Large Colophon View</span>
-        </li>
-        <li class="hide-sm">
-          <span class="mr-0 f-bold uc">fig. 06:</span>
-          <span>Small Colophon View</span>
-        </li>
-      </ul>
-    </section>
-
-    <section class="layout__img-img">
-      <figure><img class="img-shadow" src="../assets/__supersymmetry/img/05@2x.jpg" alt=""></figure>
-      <figure class="hide-sm"><img class="img-shadow" src="../assets/__supersymmetry/img/06@2x.jpg" alt=""></figure>
-      <figcaption class="mt-0 tar">fig. 05, fig. 06</figcaption>
-    </section>
-
-    <section>
-      <article>
-        <h4 class="mb-2 uc f-sm f-basismono">execution</h4>
-        <p>A big challenge for me when it comes to visual design is iteration. I tend to over-iterate and try to design for perfect. Of course designing for perfect is impossible, but that doesn't stop me from trying.</p>
-        <p>One piece I'm really proud of was the design &amp; functionality of the Colophon "page." The concept was to create a component that gets triggered by a link on the list page &amp; have motion communicate the transition from the list page to the colophon page.</p>
-      </article>
-    </section>
-
-    <section class="mt-8">
-      <figure>
-        <img class="" src="../assets/__supersymmetry/img/07@2x.jpg" alt="">
-      </figure>
-      <figcaption class="mt-0 tar">fig. 07, fig. 08</figcaption>
-    </section>
-
-    <section>
-      <ul class="figure__key">
-        <li>
-          <span class="mr-0 f-bold uc">fig. 07:</span>
-          <span>Detail View One</span>
-        </li>
-        <li>
-          <span class="mr-0 f-bold uc">fig. 08:</span>
-          <span>Detail View Two</span>
-        </li>
-      </ul>
-    </section>
-
-    <section>
-      <article>
-        <h4 class="mb-2 uc f-sm f-basismono">next steps</h4>
-        <p>As I continue to update the entries throughout the year, I'm still developing the next steps for Supersymmetry. My initial thought was to update the layout annually and perhaps expand the scope as my familiarity with Vue grows. That may still be in play as I really enjoyed working on both the subject matter &amp; the design for this project. </p>
-        <p>One thing I'd like to improve upon is the detail pages. Currently, they're rather bland and I'd like to possibly highlight more imagery. In whatever route I decide to go in, I'm proud of basically having zero experience with Vue to now using it primarily as I build simple web applications.</p>
-      </article>
-    </section>
-
-    <section class="fin tac">
-      <h2 class="mb-1 f-lg uc f-travels-med">Fin</h2>
-      <h4 class="mb-0 uc f-sm f-basismono">Read the Longform case study:</h4>
-      <h3 class="f-md f-travels-med">
-        <a class="link btn__tertiary" href="https://paper.dropbox.com/doc/Supersymmetry-vka0BSq3SfT0HDthOW0az" target="_blank">The Long Read</a>
-      </h3>
-    </section>
-
-    <section class="mb-8 uc">
-      <h4 class="mb-0 f-sm f-basismono">Next Case Study</h4>
-      <h1 style="display: inline;" class="btn__tertiary mb-1 f-xl f-mabry-med">
-        <router-link to="/info">Questions About Space</router-link>
-      </h1>
-    </section>
+    <!-- stand-alone article 📝 -->
+    <article class="copy__full">
+      <!-- <header><h4 class="mb-1 f-sm uc f-basismono">Task</h4></header> -->
+      <p>The main challenge was to create a brief repository for information of particular spaces. I wanted Supersymmetry to have brevity but highlight what makes these spaces special &amp; unique.</p>
+      <p>I wanted to structure Supersymmetry this way for the sake of brevity and maintenance, while highlighting what makes these specific spaces special (...that alliteration though 👀👏🏼).</p>
+    </article>
 
   </main>
 </template>
@@ -189,88 +77,38 @@
   p {
     font-size: 1.7rem;
     line-height: 1.4;
-    @include breakpoint(md) { font-size: 2.4rem; line-height: 1.3; }
+    @include breakpoint(md) { font-size: 2.4rem; line-height: 1.44; }
   }
 
   p + p { margin-top: 1.6rem; text-indent: 2ch; }
-  .hide-sm { display: none; @include breakpoint(mdl) { display: block; } }
 
-  .hero {
-    position: relative;
-    height: 72vh;
-    @include breakpoint(mdl) { height: 100vh; }
-  }
+  figcaption { font-size: 1.4rem; }
 
-  header {
+  // hero section
+  .hero__header {
     padding-top: 12rem;
-    @include breakpoint(mdl) { padding-top: 20rem; width: grid-width(8.4); }
-  }
-
-  .hero__meta {
-    position: absolute;
-    bottom: 3.2rem;
-    @include breakpoint(mdl) { bottom: 6.4rem; }
+    @include breakpoint(md)  { padding-top: 20rem; width: grid-width(10); }
+    @include breakpoint(mdl) { width: grid-width(8); }
   }
 
   .hero__scroll {
-    margin-top: 10.4rem;
-    @include breakpoint(mdl) { margin-top: 9.6rem; }
+    margin-top: 4.8rem;
+    @include breakpoint(md) { margin-top: 9.6rem; }
   }
 
-  .layout__img { margin: 2rem 0 4rem; }
-  .layout__img > figure { overflow: hidden; }
+  .hero__meta { margin-top: 8.8rem; }
 
-  .layout__img > figure > img {
-    margin-left: grid-width(3);
-    width: grid-width(6);
-    transform: translateY(0.5rem);
-  }
+  // img + copy sections
+  .img__full { margin: 6.4rem 0 3.2rem; }
 
-  .layout__copy-img {
-    display: flex;
-    flex-direction: column;
-    @include breakpoint(mdl) { flex-direction: row; align-items: center; margin-bottom: 4rem; }
-  }
-
-  .layout__copy-img > aside {
-    margin: 0 auto 2rem;
-    @include breakpoint(mdl) { width: grid-width(5); }
-  }
-
-  .img__wrapper {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 3rem;
-    @include breakpoint(mdl) { margin-left: grid-width(1); width: grid-width(6); }
-  }
-
-  .figure__key {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    margin: 2rem auto 4rem;
-    text-align: center;
-    @include breakpoint(mdl) { margin: 4.8rem auto 4rem; }
-  }
-
-  .layout__img-img {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    margin: 2rem auto;
-
+  .img__full > figure > img {
     @include breakpoint(mdl) {
-      flex-direction: row;
-      figure:nth-of-type(1) { padding-right: 4.8rem; width: grid-width(9); }
-      figure:nth-of-type(2) { width: grid-width(3); }
-      figcaption { width: grid-width(12); }
+      margin-left: grid-width(3);
+      width: grid-width(6);
+      transform: translateY(0.5rem);
     }
   }
 
-  .fin {
-    margin: 4rem 0 8rem;
-    @include breakpoint(mdl) { margin: 8rem 0 20rem; }
-  }
 
 </style>
 
