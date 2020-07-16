b7ed2cce59c93d407e382a9ab1555b9bc8f38dac
Matthew Pence <matt@ohiosveryown.co>
2020-07-16 11:17:21 -0400

    Setting up type scale

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h1 class="f--g">Hello World</h1>
-    <h1 class="f--f">and hello to you too...</h1>
+    <h1 class="mb-0 f--f fs--lg">My name is Matt.</h1>
+    <h2 class="mb-4 f--g fs--md">I’m a product designer living in Atlanta, Ga. Presently I’m researching design systems & machine learning.</h2>
+    <p class="f--f">Creating an all new editing experience with maximum flexibility to make designing easier.</p>
   </div>
 </template>
 
 
 <style lang='scss' scoped>
-
+  div { margin-top: 4rem; margin-left: 4rem; width: 40%; }
 </style>
 
 
