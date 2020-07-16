4da5ae289394c756bca82eb919fb68b992a098b7
Matthew Pence <matt@ohiosveryown.co>
2020-07-16 12:06:25 -0400

    Set type scale

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <h1 class="mb-0 f--f fs--lg">My name is Matt.</h1>
-    <h2 class="mb-4 f--g fs--md">I’m a product designer living in Atlanta, Ga. Presently I’m researching design systems & machine learning.</h2>
-    <p class="f--f">Creating an all new editing experience with maximum flexibility to make designing easier.</p>
+    <h5 class="mb-6 uc f--f fs--sm">
+      Navigation
+      <span>
+<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M11.6569 6L6.8081 10.8488L6 11.6569L5.99997 11.6569L0.343121 6.00002L1.15124 5.1919L5.46212 9.50278L5.46212 0.932321L6.48596 0.932321L6.48596 9.55465L10.8487 5.19188L11.6569 6Z" fill="black"/>
+</svg>
+
+      </span>
+    </h5>
+    <h1 class="mb-2 f--c fs--lg">My name is Matt.</h1>
+    <h2 class="mb-9 f--g fs--md">I’m a product designer living in Atlanta, Ga. Presently I’m researching design systems & machine learning.</h2>
+    <h4 class="mb-6 uc f--f fs--sm">Case Studies</h4>
+
+    <h3 class="mb-2 f--g fs--md">Mailchimp First-time Send</h3>
+    <p class="mb-4 f--c">Creating an all new editing experience with maximum flexibility to make designing easier.</p>
   </div>
 </template>
 
 
 <style lang='scss' scoped>
   div { margin-top: 4rem; margin-left: 4rem; width: 40%; }
+
+  h5 {
+    position: absolute;
+    top: 2rem; right: 2rem;
+  }
+
+  span {
+    display: inline;
+  }
 </style>
 
 
