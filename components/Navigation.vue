9c03f0f05767a15f706339328c77017ea14be49b
Matthew Pence <matt@ohiosveryown.co>
2020-07-21 19:29:55 -0400

    nav logic

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,13 +2,14 @@
   <nav class="width">
     <span class="mt-0 op-4 fs--sm f--f uc">ovo / 3.5</span>
     <header @click = 'isOpen = !isOpen'>
-      <span class="mt-0 fs--sm f--f uc">Navigation</span>
+      <span class="label mt-0 fs--sm f--f uc">Navigation</span>
       <svg :class = "[ isOpen ? 'arrow-opened' : 'arrow-closed' ]" class="ml-0" width="12" height="12" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.444 0h1.112v7.848c.159-.243.331-.478.515-.702C8.043 5.97 9.435 5 11 5v1.111c-1.102 0-2.21.698-3.071 1.743-.862 1.044-1.373 2.311-1.373 3.257H5.444c0-.946-.511-2.213-1.373-3.257C3.21 6.809 2.101 6.11 1 6.11V5c1.565 0 2.957.969 3.929 2.146.184.224.356.459.515.702V0z" fill="#000"/></svg>
     </header>
 
       <div
         class="menu"
         :class = "[ isOpen ? 'menu-opened' : 'menu-closed' ]"
+        @mouseleave = 'isOpen = false'
       >
         <section :class = "[ isOpen ? 'menu-items-opened' : 'menu-items-closed' ]">
           <ul @click = 'isOpen = !isOpen'>
@@ -58,6 +59,8 @@
   }
 
   header { display: flex; cursor: pointer; }
+
+  @media (pointer: fine) { .label:hover { text-decoration: underline; }}
 
   .menu {
     position: absolute;
@@ -114,6 +117,7 @@
     bottom: 0;
     padding: 2.4rem 1.8rem 1.8rem;
     backdrop-filter: blur(18px);
+    background: rgba(255,255,255,.3);
     h6 { font-size: 1.2rem; }
     @include breakpoint(md) { display: none; }
   }
@@ -135,6 +139,12 @@
       return {
         works,
         isOpen: false,
+      }
+    },
+
+    methods: {
+      noScroll() {
+        document.documentElement.style.overflow = 'hidden'
       }
     },
   }
