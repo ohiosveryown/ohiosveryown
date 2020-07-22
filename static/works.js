c269caa3fc19a5188105bf977f0f65d140b4dbe9
Matthew Pence <matt@ohiosveryown.co>
2020-07-21 22:06:33 -0400

    Netlify host video

--- static/works.js
+++ static/works.js
@@ -5,7 +5,7 @@ const works = [
     name: 'Mailchimp First-time Send',
     excerpt: "Creating an all new editing experience with maximum flexibility to make designing easier",
     excerptSmall: "Creating an all-new editing experience to make designing campaigns easier",
-    video: 'http://ohiosveryown.co/img/principles.mp4',
+    video: './img/principles.mp4',
     img: './img/navigation/mailchimp.jpg',
     link: 'test',
     background: 'var(--mailchimp)'
@@ -16,7 +16,7 @@ const works = [
     name: 'Menu.black',
     excerpt: "Bringing awareness and celebrating the diversity of Atlanta and it's food",
     excerptSmall: "Celebrating the diversity of Atlanta's culture and food",
-    video: 'http://ohiosveryown.co/img/principles.mp4',
+    video: './img/principles.mp4',
     img: './img/navigation/menu.jpg',
     link: '',
     background: '#D2EDD4'
@@ -27,7 +27,7 @@ const works = [
     name: 'ShootProof Galleries',
     excerpt: "Rethinking how users share, sell and organizer their photos and albums",
     excerptSmall: "Rethinking how users share and sell their photos and albums",
-    video: 'http://ohiosveryown.co/img/principles.mp4',
+    video: './img/principles.mp4',
     img: './img/navigation/shootproof.jpg',
     link: '',
     background: 'var(--shootproof)'
@@ -38,7 +38,7 @@ const works = [
     name: 'Supersymmetry',
     excerpt: "A self-initiated project showcasing architecture  & structural engineering",
     excerptSmall: "A project showcasing architecture  & structural engineering",
-    video: 'http://ohiosveryown.co/img/principles.mp4',
+    video: './img/principles.mp4',
     img: './img/navigation/supersymmetry.jpg',
     link: '',
     background: '#F8F5EC'
