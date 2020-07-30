abacad4399880706c31234e271e40b6c6b105c72
Matthew Pence <matt@ohiosveryown.co>
2020-07-30 00:30:01 -0400

    Detail layout

--- layouts/work.vue
+++ layouts/work.vue
@@ -11,28 +11,53 @@
 
   .work {
 
+    p {
+      margin-bottom: 1rem;
+      font-family: 'Founders', var(--system-ui);
+      font-size: 2.2rem;
+    }
+
+    p + p { text-indent: 3ch; }
+
+    .fs--lg {
+      font-size: 2.6rem;
+      @include breakpoint(md) { font-size: 3rem; }
+    }
+
+    .fs--md {
+      font-size: 2rem;
+      @include breakpoint(md) { font-size: 2.4rem; }
+    }
+
+    .title { margin-bottom: 4rem; }
+
     .meta {
       margin: 0 auto;
-      padding-top: 14rem;
+      padding-top: 16rem;
       @include breakpoint(md) { width: grid-width(9); }
       @include breakpoint(mdl) { width: grid-width(5.5); }
     }
 
-    .fs--lg {
-      font-size: 2.8rem;
-      @include breakpoint(md) { font-size: 3rem; }
-    }
-
     .hero {
-      margin: 0 auto;
+      margin: 4rem auto 0;
       padding: 4rem 4rem 0;
       background: var(--mailchimp);
       overflow: hidden;
-      @include breakpoint(md) { padding: 8rem 8rem 0; }
+
+      @include breakpoint(md) { margin: 6.4rem auto 0; padding: 8rem 8rem 0; }
       @include breakpoint(mdl) { padding: 12rem 16rem 0; }
+
       img { box-shadow: 0px 4px 64px rgba(12,0,0,.08); }
     }
 
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 4rem auto 8rem;
+
+      @include breakpoint(mdl) { width: grid-width(6); }
+    }
   }
 </style>
 
--- pages/mc-send.vue
+++ pages/mc-send.vue
@@ -6,12 +6,12 @@
         <h1 class="mb-2 fs--lg f--c">Mailchimp First Time Send</h1>
         <h2 class="mb-3 fs--md f--g">Creating an all new editing experience with maximum flexibility to make designing easier.</h2>
 
-        <h4 class="uc fs--sm f--f">
+        <h4 class="mb-0 uc fs--sm f--f">
           <span class="pt-0 op-5">user experience design</span>
           2019–2020
         </h4>
 
-        <h4 class="mb-8 uc fs--sm f--f">
+        <h4 class="uc fs--sm f--f">
           <span class="pt-0 op-5">url</span>
           mailchimp.com
         </h4>
@@ -21,13 +21,15 @@
         <div class="hero">
           <img src="~/static/img/mc-send/01@2x.jpg" alt="mailchimp homepage">
         </div>
-        <figcaption><span>Fig</span>Nº01</figcaption>
+        <figcaption class="mt-2 tar uc fs--sm f--f"><span class="mr-0 pt-0 op-5">Fig</span>Nº01</figcaption>
       </figure>
     </header>
 
     <!-- section -->
-    <section class="intro">
-      <h3>Introduction</h3>
+    <section>
+      <header>
+        <h3 class="mb-4 fs--md f--g">Introduction</h3>
+      </header>
       <article>
         <p>As we started the redesign, I helped scope the project as well as be a product owner to help define what all we'd be incorporating.</p>
         <p>Throughout the life cycle of the project redesign, I played a large role in determining not only the visual design language, but also what features would be added and depreciated.</p>
@@ -36,25 +38,28 @@
     </section>
 
     <!-- caption -->
-    <section class="caption">
-      <h4>
-        <span>Fig</span>
+    <section class="caption tac">
+      <h4 class="mb-0 uc fs--sm f--f">
+        <span class="pt-0 op-5">Fig</span>
         Nº01 ShootProof Galleries v3
       </h4>
 
-      <h4>
-        <span>Fig</span>
+      <h4 class="uc fs--sm f--f">
+        <span class="pt-0 op-5">Fig</span>
         Nº02 ShootProof Galleries v2
       </h4>
     </section>
 
     <figure>
       <img src="~/static/img/mc-send/02@2x.jpg" alt="mailchimp checklist">
+      <figcaption class="mt-2 tar uc fs--sm f--f"><span class="mr-0 pt-0 op-5">Fig</span>Nº02</figcaption>
     </figure>
 
     <!-- section -->
-    <section class="intro">
-      <h3>Conclusion / Next Steps</h3>
+    <section>
+      <header>
+        <h3 class="mb-4 fs--md f--g">Conclusion / Next Steps</h3>
+      </header>
       <article>
         <p>As we started the redesign, I helped scope the project as well as be a product owner to help define what all we'd be incorporating.</p>
         <p>Throughout the life cycle of the project redesign, I played a large role in determining not only the visual design language, but also what features would be added and depreciated.</p>
