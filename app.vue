b00fecda9bcd7d390c2adb4dce6e3d4c8d5472b7
ohiosveryown <matt@ohiosveryown.co>
2024-06-14 16:35:35 -0400

    refactor: Update meta tags for SEO optimization

--- app.vue
+++ app.vue
@@ -5,27 +5,45 @@
 </template>
 
 <script setup lang="ts">
-  useSeoMeta({
-    description:
-      "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
-
-    // open graph
-    ogType: "website",
-    ogSiteName: "ohiosveryown",
-    ogTitle: "ohiosveryown",
-    ogDescription:
-      "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
-    ogUrl: "https://ohiosveryown.com",
-    ogImage:
-      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
-
-    // twitter
-    twitterCard: "summary_large_image",
-    twitterTitle: "ohiosveryown",
-    twitterUrl: "https://ohiosveryown.com",
-    twitterDescription:
-      "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
-    twitterImage:
-      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
+  useHead({
+    meta: [
+      {
+        name: "description",
+        content:
+          "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
+      },
+      // Open Graph
+      { property: "og:type", content: "website" },
+      { property: "og:site_name", content: "ohiosveryown" },
+      { property: "og:title", content: "ohiosveryown" },
+      {
+        property: "og:description",
+        content:
+          "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
+      },
+      { property: "og:url", content: "https://ohiosveryown.com" },
+      {
+        property: "og:image",
+        content:
+          "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
+      },
+      // Twitter
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:title", content: "ohiosveryown" },
+      { name: "twitter:url", content: "https://ohiosveryown.com" },
+      {
+        name: "twitter:description",
+        content:
+          "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
+      },
+      {
+        name: "twitter:image",
+        content:
+          "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
+      },
+      // Robots
+      { name: "robots", content: "index,follow" },
+    ],
+    link: [{ rel: "canonical", href: "https://ohiosveryown.com" }],
   })
 </script>
