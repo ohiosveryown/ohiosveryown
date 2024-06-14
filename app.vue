e01e3ed6d1d485514e1d0054fa79135ef011d5c0
ohiosveryown <matt@ohiosveryown.co>
2024-06-14 16:28:21 -0400

    add og site name

--- app.vue
+++ app.vue
@@ -11,12 +11,13 @@
 
     // open graph
     ogType: "website",
+    ogSiteName: "ohiosveryown",
     ogTitle: "ohiosveryown",
     ogDescription:
       "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
     ogUrl: "https://ohiosveryown.com",
     ogImage:
-      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme.webp?updatedAt=1718396304149",
+      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
 
     // twitter
     twitterCard: "summary_large_image",
@@ -25,6 +26,6 @@
     twitterDescription:
       "ohiosveryown is the web presence and portfolio of work for Matthew Pence – a designer living in Atlanta, Ga.",
     twitterImage:
-      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme.webp?updatedAt=1718396304149",
+      "https://ik.imagekit.io/ohiosveryown/ovo--3.7/readme__sm.webp?updatedAt=1718396522129",
   })
 </script>
