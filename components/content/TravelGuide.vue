797624b8cf348ebfb636b1dd175f36be6cac3433
ohiosveryown <matt@ohiosveryown.co>
2024-07-20 10:38:25 -0400

    expand / collapse bare styling working

--- components/content/TravelGuide.vue
+++ components/content/TravelGuide.vue
@@ -1,36 +1,22 @@
 <template>
-  <menu
-    ref="menuRef"
-    :class="{ 'mini-player': state.isPlaying }"
-  >
-    <header
-      ref="headerRef"
-      :style="{ gridTemplateRows: state.isPlaying ? '1fr' : '' }"
-    >
-      <div class="content">
+  <menu>
+    <section>
+      <div class="section-content">
         <video
           ref="videoRef"
           playsinline
           controls
           src="https://ik.imagekit.io/ohiosveryown/ovo--3.7/about/os.mp4"
-        ></video>
-        <span
-          ref="spanRef"
-          :class="{ collapse: state.isPlaying }"
-        >
-          <span class="span-content">
-            <span class="padding">👋 Welcome, start your journey here.</span>
-          </span>
-        </span>
+          @play="onPlay"
+        />
+        <p>
+          Experience the website travel guide, brought to life in rich
+          high-definition video, narrated by me: Matt.
+        </p>
       </div>
-    </header>
-    <footer
-      ref="footerRef"
-      :class="{ collapse: state.isPlaying }"
-    >
-      <div class="footer-content">
-        <div class="padding">This is the footer</div>
-      </div>
+    </section>
+    <footer>
+      <div class="footer-content">Footer</div>
     </footer>
   </menu>
 </template>
@@ -47,117 +33,50 @@
     padding: 0.8rem;
     width: 400px;
     color: #fff;
-    // background: #fff;
     background: url("https://res.cloudinary.com/dn1q8h2ga/image/upload/v1721321971/ovo-3.7/global/guide-bg-1_3x_mddlit.webp")
       no-repeat center center;
     background-size: 108%;
     box-shadow: 0px 100px 80px 0px rgba(0, 0, 0, 0.08),
       0px 22.336px 16px 0px rgba(0, 0, 0, 0.06),
       0px 6.65px 5px 0px rgba(0, 0, 0, 0.05);
-    // opacity: 0;
-    // transform: translateY(1rem);
-    transition: var(--ease--qubic);
-    // overflow: hidden;
   }
 
-  .mini-player:hover {
-    width: 600px;
-  }
-
-  .mini-player video {
-    opacity: 1 !important;
-    transform: translateY(0) !important;
-  }
-
-  menu:hover header {
+  menu:hover section {
     grid-template-rows: 1fr;
   }
 
-  header {
+  menu:hover .footer-content {
+    opacity: 1;
+    border: 0.5px solid rgba(255, 255, 255, 0.04);
+    background: radial-gradient(42% 84% at 50% -16%, #555 0%, #141414 100%);
+    transition: all 300ms ease;
+    box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.76),
+      0px -1px 0px 0px #000 inset,
+      0px 0.5px 1px 0px rgba(255, 255, 255, 0.56) inset;
+  }
+
+  section {
     display: grid;
     grid-template-rows: 0fr;
-    transition: var(--ease--qubic);
     overflow: hidden;
-  }
-
-  .content {
-    min-height: 0;
-  }
-
-  video {
-    display: flex;
-    border-radius: 5px;
-    border: 1px solid rgba(255, 255, 255, 0.12);
-  }
-
-  span {
-    display: block;
-  }
-
-  span {
-    display: grid;
-    grid-template-rows: 1fr;
     transition: var(--ease--qubic);
   }
 
   footer {
     display: grid;
     grid-template-rows: 1fr;
-    // color: #fff;
-    // background: #000;
-    transition: var(--ease--qubic);
   }
 
-  menu:hover .span-content,
-  menu:hover video {
-    opacity: 1;
-    transform: translateY(0);
-  }
-
-  menu:hover video {
-    transition: transform 700ms cubic-bezier(0.8, 0, 0.16, 1) 150ms,
-      opacity 1ms ease;
-  }
-
-  menu:hover .span-content {
-    transition: transform 700ms cubic-bezier(0.8, 0, 0.16, 1) 300ms,
-      opacity 1ms ease;
-  }
-
-  .span-content,
+  .section-content,
   .footer-content {
     min-height: 0;
   }
 
-  .span-content,
-  video {
-    opacity: 1;
-    transform: translateY(3.2rem);
-    transition: transform 1ms ease 700ms, opacity 500ms ease;
-  }
-
-  // video {
-  //   transform: translateY(3.2rem) scaleY(0);
-  //   transform-origin: bottom;
-  // }
-
-  .padding {
-    padding: 32px;
-  }
-
-  .collapse {
-    grid-template-rows: 0fr;
-    .footer-content,
-    .span-content {
-      min-height: 0;
-    }
-  }
-
-  .dismiss-player {
-    filter: blur(6px);
-    transform: translateY(175vh) scaleX(0.45) scaleY(4.5);
-    transition: opacity 700ms ease 100ms, filter 500ms ease,
-      transform 600ms cubic-bezier(0.8, 0, 0.16, 1);
+  .footer-content {
+    display: flex;
+    border-radius: 9px;
+    transition: all 300ms ease 700ms;
+    opacity: 0;
   }
 </style>
 
