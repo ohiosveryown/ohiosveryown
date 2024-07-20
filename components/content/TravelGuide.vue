dfccc0b61e6c2d7d5dfd2dd5ff585c655aa8761c
ohiosveryown <matt@ohiosveryown.co>
2024-07-20 13:29:35 -0400

    footer styles ✅

--- components/content/TravelGuide.vue
+++ components/content/TravelGuide.vue
@@ -17,7 +17,7 @@
       <div class="collapsable">
         <article>
           <p class="title">Your travel guide</p>
-          <p class="caption thin">
+          <p class="thin caption">
             Experience the website travel guide, brought to life in rich
             high-definition video, narrated by me: Matt.
           </p>
@@ -27,7 +27,38 @@
 
     <footer ref="footerRef">
       <div class="collapsable">
-        <div class="footer-content">Footer</div>
+        <div class="footer-content">
+          <div class="thumbnails">
+            <img
+              class="thumbnail--cover"
+              src="https://res.cloudinary.com/dn1q8h2ga/image/upload/v1721314748/ovo-3.7/global/guide--cover_2x_cakm6t.webp"
+              alt=""
+            /><img
+              class="thumbnail--interior"
+              src="https://res.cloudinary.com/dn1q8h2ga/image/upload/v1721312846/ovo-3.7/global/guide--writing_2x_hu5ei6.webp"
+              alt=""
+            />
+          </div>
+          <article>
+            <span class="footer-title">Video travel guide</span>
+            <span class="thin footer-caption">edition 3.7</span>
+          </article>
+
+          <figure class="icon">
+            <svg
+              width="14"
+              height="14"
+              fill="none"
+            >
+              <path
+                fill="#fff"
+                fill-rule="evenodd"
+                d="M10.42 1.06a6.493 6.493 0 0 1 3.406-.14.23.23 0 0 1 .174.22v10.148c0 .066-.03.128-.081.17a.22.22 0 0 1-.184.04 6.479 6.479 0 0 0-5.861 1.669.216.216 0 0 1-.239.053.216.216 0 0 1-.137-.201V3.063c0-.057.02-.113.058-.155a6.496 6.496 0 0 1 2.865-1.848ZM0 1.14A.23.23 0 0 1 .174.92a6.49 6.49 0 0 1 6.27 1.994.235.235 0 0 1 .058.155v9.956a.216.216 0 0 1-.137.201.216.216 0 0 1-.239-.052 6.476 6.476 0 0 0-5.861-1.67.22.22 0 0 1-.184-.04.218.218 0 0 1-.081-.17V1.14Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </figure>
+        </div>
       </div>
     </footer>
   </menu>
@@ -45,7 +76,6 @@
     border-radius: 21px;
     padding: 0.8rem;
     width: 40rem;
-    text-align: center;
     color: #fff;
     background: url("https://res.cloudinary.com/dn1q8h2ga/image/upload/v1721321971/ovo-3.7/global/guide-bg-1_3x_mddlit.webp")
       no-repeat center center;
@@ -58,10 +88,8 @@
   header,
   section {
     display: grid;
-    // grid-template-rows: 0fr;
     grid-template-rows: 0fr;
     transition: all var(--ease--qubic);
-    // overflow: hidden;
   }
 
   section {
@@ -84,7 +112,7 @@
     opacity: 0;
     transform: translateY(6.4rem) scale(1);
     transform-origin: top;
-    transition: transform 200ms ease 600ms, opacity 400ms ease 300ms;
+    transition: transform 200ms ease 600ms, opacity 320ms ease 300ms;
     will-change: opacity, transform;
     overflow: hidden;
   }
@@ -93,7 +121,7 @@
     min-height: 0;
   }
 
-  // styles
+  // interior styles
   section article {
     display: flex;
     flex-direction: column;
@@ -125,12 +153,60 @@
   .footer-content {
     display: flex;
     align-items: center;
-    gap: 1.2rem;
+    gap: 1.4rem;
     border-radius: 13px;
-    // border: 1px solid rgba(0, 0, 0, 1);
     padding: 1.2rem 1.2rem 1.2rem 1.8rem;
     background: radial-gradient(42% 84% at 50% -16%, #555 0%, #141414 100%);
+    box-shadow: 0px -2px 2px 0.5px #000 inset,
+      0px 0px 1.5px 1px rgba(255, 255, 255, 0.4) inset;
     transition: all 300ms ease;
+  }
+
+  .footer-content article {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+
+  .footer-title {
+    font-weight: 532;
+  }
+
+  .footer-caption {
+    letter-spacing: 0.25px;
+  }
+
+  .thumbnails {
+    display: flex;
+  }
+
+  .thumbnails img {
+    border-radius: 5px;
+    width: auto;
+    height: 4.8rem;
+    box-shadow: var(--shadow--xs);
+    transform-origin: bottom;
+    transition: transform 500ms ease;
+  }
+
+  .thumbnail--cover {
+    z-index: var(--z1);
+    transform: rotate(-10deg);
+  }
+
+  .thumbnail--interior {
+    margin-left: -3.2rem;
+    transform: rotate(10deg) translateX(-1rem) translateY(0.1rem);
+  }
+
+  .footer-content .icon {
+    display: grid;
+    place-items: center;
+    border-radius: 6px;
+    border: 0.6px solid rgba(255, 255, 255, 0.14);
+    width: 2.6rem;
+    height: 2.6rem;
+    background: rgba(255, 255, 255, 0.2);
   }
 
   // hover styles
@@ -158,9 +234,17 @@
     transition: transform 500ms ease 400ms;
   }
 
+  menu:hover .thumbnail--cover {
+    transform: rotate(-15deg) translateX(-0.25rem);
+  }
+
+  menu:hover .thumbnail--interior {
+    transform: rotate(15deg) translateX(-0.5rem);
+  }
+
   // onPlay styles
   .mini-player:hover {
-    width: 60rem;
+    width: 62rem;
   }
 
   .mini-player video {
