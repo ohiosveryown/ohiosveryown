0ff7620efd65d557b4f7d8beca87ffe17747300c
ohiosveryown <matt@ohiosveryown.co>
2026-05-15 13:01:01 -0400

    update nuxt configuration, add SSR support, and enhance index page styling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Hello World!</div>
+  <div>
+    <h1>Hello World!</h1>
+    this is body text...
+  </div>
 </template>
+
+<style scoped lang="scss">
+  h1 {
+    @include breakpoint(md) {
+      color: red;
+    }
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  experimental: {
+    // Workaround for https://github.com/nuxt/nuxt/issues/34957
+    // Remove once a Nuxt release ships the fix from PR #34959.
+    viteEnvironmentApi: true,
+  },
+  css: [
+    '~/style/type.scss',
+    '~/style/reset.scss',
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/style/grid.scss" as *;`,
+        },
+      },
+    },
+  },
 })
