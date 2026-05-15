22820e554b01b3ffb5472ea88989ab39667837dc
ohiosveryown <matt@ohiosveryown.co>
2026-05-15 13:04:19 -0400

    remove SSR configuration and experimental settings from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
-  experimental: {
-    // Workaround for https://github.com/nuxt/nuxt/issues/34957
-    // Remove once a Nuxt release ships the fix from PR #34959.
-    viteEnvironmentApi: true,
-  },
   css: [
     '~/style/type.scss',
     '~/style/reset.scss',
